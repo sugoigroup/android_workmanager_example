3dbb0496cf0427cbdb6c710987f8efb3a1fa1d4e
kimtaeho <>
2021-01-31 14:02:33 +0900

    2.백그라운드 알림창으로 통지가 오는지 확인

--- app/src/main/java/com/sugoijapaneseschool/workmanagerexample/UpladWorker.java
+++ app/src/main/java/com/sugoijapaneseschool/workmanagerexample/UpladWorker.java
@@ -1,11 +1,13 @@
 package com.sugoijapaneseschool.workmanagerexample;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -23,8 +25,9 @@ public class UpladWorker extends Worker {
         while (count < 10) {
             count++;
             try {
-                Log.i("worker", "now background" + count);
-                Thread.sleep(1000);
+               // Log.i("worker", "now background" + count);
+                showNotification("worker", "now background" + count);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -32,5 +35,24 @@ public class UpladWorker extends Worker {
 
         return Result.success();
 
+    }
+
+    private void showNotification(String task, String desc) {
+        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        String channelId = "my_channel";
+        String channelName = "my_name";
+        // Oreo 부터는 노티에 알림하려면 채널이 있어야 한다.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new
+                    NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(getApplicationContext(), channelId)
+                        .setContentTitle(task)
+                        .setContentText(desc)
+                        .setSmallIcon(R.mipmap.ic_launcher);
+        manager.notify(1, builder.build());
     }
 }
