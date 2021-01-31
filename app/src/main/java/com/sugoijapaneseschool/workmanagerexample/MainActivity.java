4bd3b10667b4f09879a317f124d68066b44c1f3e
kimtaeho <>
2021-01-31 16:45:19 +0900

    4. 같은 workRequest에 대해 동시에 여러번 실행되지 않도록 하기

--- app/src/main/java/com/sugoijapaneseschool/workmanagerexample/MainActivity.java
+++ app/src/main/java/com/sugoijapaneseschool/workmanagerexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.sugoijapaneseschool.workmanagerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.Constraints;
+import androidx.work.ExistingWorkPolicy;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
@@ -13,13 +14,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView tv = (TextView) findViewById(R.id.hello);
         tv.setOnClickListener(e -> {
-            WorkManager.getInstance(this).cancelAllWorkByTag(CANCEL_ME);
+            startQueue();
         });
+        startQueue();
+    }
+
+    private void startQueue() {
 
         // 충전중일때만 worker 가 실행되도록 제한을 건다.
         Constraints constraints = new Constraints.Builder()
@@ -27,12 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         // 이번 한번만 일하도록 한다.
-        OneTimeWorkRequest oneTimeWorkRequest =  new OneTimeWorkRequest.Builder(UpladWorker.class)
+        final OneTimeWorkRequest  oneTimeWorkRequest =  new OneTimeWorkRequest.Builder(UpladWorker.class)
                 .setConstraints(constraints)
                 .addTag(CANCEL_ME)
                 .build();
 
         // 매니져에게 일할거라고 등록한다.
-        WorkManager.getInstance(this).enqueue(oneTimeWorkRequest);
+        //
+        WorkManager.getInstance(this)
+                .beginUniqueWork("iamUnique", ExistingWorkPolicy.APPEND_OR_REPLACE, oneTimeWorkRequest)
+                .enqueue();
     }
 }
